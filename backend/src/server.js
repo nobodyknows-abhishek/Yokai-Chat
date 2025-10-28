3e54961afbad658904bd05d8515a3355f94e777d
Abhishek Verma <130924459+nobodyknows-abhishek@users.noreply.github.com>
2025-10-28 21:19:46 +0530

    Update CORS origin to use environment variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
