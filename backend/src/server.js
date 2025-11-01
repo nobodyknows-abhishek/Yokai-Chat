2d60c4ada133fcb90795a9f1ef1c58c9f308a744
Abhishek Verma <130924459+nobodyknows-abhishek@users.noreply.github.com>
2025-11-01 19:50:21 +0530

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,15 +15,16 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
+const FRONTEND_URL = (
+  process.env.FRONTEND_URL || "http://localhost:5173"
+).replace(/\/$/, "");
+
 app.use(
   cors({
-    origin:process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: FRONTEND_URL,
+    credentials: true, 
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
 
